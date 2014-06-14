018c398b8a86ee916817585fce8baf5f11ed266d
matanco <matan@numgames.com>
2014-06-14 20:30:19 +0300

    improve code

--- lib/math_lib.rb
+++ lib/math_lib.rb
@@ -5,14 +5,10 @@ class String
     true if Float(self) rescue false
   end
 
-  def detect_coefficient
-    self.each_char do |ch|
-      if ch.is_number?
-        next
-      else
-        return ch.to_s
-      end
-    end
+  ##= move all expressions to same side in equation and equal it to 0
+  def orginize_equation
+    gsub!(' ','')
+    ##TODO:: orginaize 
   end
 end
 
@@ -22,7 +18,11 @@ module MathLib
 
     ##= Validate the all input are numbers and convert them to integer
     def self.validate_input(input)
-      input.to_s.is_number? ? input.to_i : raise('MathLib handle only numbers.')
+      input.to_s.is_number? ? input.to_i : raise('MathLib:: handle only numbers.')
+    end
+
+    def self.validate_linear_equation(input)
+      input.index('=').present? ? true : raise('MathLib:: invalid linear equation')
     end
 
   end
@@ -32,7 +32,7 @@ module MathLib
     def initialize
     end
 
-    ##= Calculate quadratic_equation
+    ##= Calculate quadratic equation
     def quadratic_equation(a,b,c)
       a = MathLib::MathValidator.validate_input(a)
       b = MathLib::MathValidator.validate_input(b)
@@ -41,23 +41,30 @@ module MathLib
       delta = (b * b) - (4 * a * c)
 
       if (delta < 0)
-        puts "Delta is complex number"
+        puts 'Delta is complex number'
 
         return 0
       elsif (delta == 0)
-        puts "quadratic equation has has only 1 answer"
+        puts 'quadratic equation has has only 1 answer'
         root1 = -b / (2* a);
         puts "Roots of quadratic equation is: #{root1}"
 
         return 0
       elsif (delta > 0)
-        puts "quadratic equation has real numbers"
+        puts 'quadratic equation has real numbers'
         root1 = ( -b + Math.sqrt(delta)) / (2 * a)
         root2 = ( -b - Math.sqrt(delta)) / (2 * a)
         puts "Roots of quadratic equation are: #{root1} || #{root2}"
 
         return 0
       end
+    end
+
+    ##= Calculate linear equation
+    ##= Syntax:: 3x + 7 = 4x * 6^2
+    ##= Syntax:: 2x = 2
+    def linear_equation(equation)
+      #TODO:: calculate
     end
 
   end
